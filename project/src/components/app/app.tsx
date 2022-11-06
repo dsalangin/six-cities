97f585c32427d95e905a758c4e9f6103158158b8
dsalangin <dsalangin@74.ru>
2022-11-06 13:11:51 +0300

    Добавляет страницы и компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,23 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import PropertyScreen from '../property-screen/property-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+
+type AppProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppProps): JSX.Element {
+  return (
+    <>
+      <MainScreen offersCount={offersCount}/>
+      <LoginScreen/>
+      <FavoritesScreen/>
+      <PropertyScreen/>
+      <NotFoundScreen/>
+    </>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Seting = {
+  OffersCount: 5
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App offersCount={Seting.OffersCount}/>
   </React.StrictMode>,
 );
