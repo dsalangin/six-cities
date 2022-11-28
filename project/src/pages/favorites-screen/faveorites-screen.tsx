bda548b7129f1c65c5c360499418e6e4a646976e
dsalangin <dsalangin@74.ru>
2022-11-28 11:29:20 +0300

    Создает компонент FavoritesEmpty

--- project/src/pages/favorites-screen/faveorites-screen.tsx
+++ project/src/pages/favorites-screen/faveorites-screen.tsx
@@ -15,7 +15,7 @@ function FavoritesScreen({offers}: FavoritesScreenProps): JSX.Element {
 
       <Header />
 
-      <main className="page__main page__main--favorites">
+      <main className="page__main page__main--favorites ">
         <div className="page__favorites-container container">
           <section className="favorites">
             <h1 className="favorites__title">Saved listing</h1>
