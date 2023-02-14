7b553eba166ac0cac774cd72aad7260df6c970fd
dsalangin <dsalangin@74.ru>
2023-02-14 19:12:47 +0300

    Вынес список коментариев в отдельный компонент

--- project/src/components/review-list/review-list.tsx
+++ project/src/components/review-list/review-list.tsx
@@ -1,34 +1,21 @@
-import type {Review} from '../../types/review';
+import { useAppSelector } from '../../hooks';
+import { getReviews } from '../../store/offers-data/selectors';
+import ReviewCard from '../review-card/review-card';
 
 
-type ReviewListProps = {
-  review: Review;
-}
-function ReviewList ({review}: ReviewListProps): JSX.Element {
+function ReviewList (): JSX.Element {
+
+  const reviews = useAppSelector(getReviews);
 
   return (
-    <li className="reviews__item">
-      <div className="reviews__user user">
-        <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src={review.user.avatarUrl} width="54" height="54" alt="Reviews avatar"/>
-        </div>
-        <span className="reviews__user-name">
-          {review.user.name}
-        </span>
-      </div>
-      <div className="reviews__info">
-        <div className="reviews__rating rating">
-          <div className="reviews__stars rating__stars">
-            <span style={{width: `${100 / 5 * Math.round(review.rating)}%`}}></span>
-            <span className="visually-hidden">Rating</span>
-          </div>
-        </div>
-        <p className="reviews__text">
-          {review.comment}
-        </p>
-        <time className="reviews__time" dateTime="2019-04-24">{review.date}</time>
-      </div>
-    </li>
+    <>
+      <h2 className='reviews__title'>Reviews &middot; <span className='reviews__amount'>{reviews.length}</span></h2>
+      <ul className='reviews__list'>
+        {reviews.map((review) => (
+          <ReviewCard review={review} key={review.id} />
+        ))}
+      </ul>
+    </>
   );
 }
 
